4fcd325a8d26219f89abe594919a442e0b1e2584
Prince Gulia <princegulia170306@gmail.com>
2026-06-25 10:36:11 +0530

    add cors

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
 require('./config/db');
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
   }
 });
-
-app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
 app.use('/auth', authRoutes);
